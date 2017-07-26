e94c885726a2e7d025e6a383ba539bf0c040bbc3
Larry Cherry <larry.cherry.0406@gmail.com>
2017-07-26 13:30:46 -0500

    created a resume object

---
+++ resume.js
@@ -0,0 +1,11 @@
+var Resume = function(){
+}
+
+Resume.prototype.name = "Larry Cherry"
+
+Resume.prototype.contact = { 
+  "github":"github.com/larry-cherry",
+  "email":"larry.cherry.0406@gmail.com",
+  "phone":"(832) 613-8302",
+  "linkedin":"https://www.linkedin.com/in/larry-cherry/"
+}
