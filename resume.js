18f665e47174c5b24cc1709a408f02146c871f63
Larry Cherry <larry.cherry.0406@gmail.com>
2017-07-28 14:06:26 -0500

    Added more to resume class

--- resume.js
+++ resume.js
@@ -9,3 +9,18 @@ Resume.prototype.contact = {
   "phone":"(832) 613-8302",
   "linkedin":"https://www.linkedin.com/in/larry-cherry/"
 }
+
+Resume.prototype.summary = "I am a web developer looking for a new challenge. I am proficient in Ruby, Javascript, Agile development, Test driven development, and Git workflow."
+
+Resume.prototype.skills = {
+  "WEB DEVELOPMENT":"Ruby, HTML, CSS, JavaScript, JQuery, Rspec, Jasmine, Git, Bootstrap, SQlite, PostgreSQL",
+  "SYSTEM ADMIN":"Active Directory, Microsoft Exchange, Google Apps"
+}
+
+Resume.prototype.projects = {
+  "RAILS API":"",
+  "WEATHER APP":"",
+  "QUOTE MACHINE":"",
+  "BUZZBUS":"",
+  "CROWDPLAYWEB":""
+}
