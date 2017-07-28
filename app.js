226068250807a0af1200f76497f7dfe718d1ca85
Larry Cherry <larry.cherry.0406@gmail.com>
2017-07-28 14:23:13 -0500

    added additional functions

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 
 var resume = new Resume()
+var p = document.createElement("p");
 
-document.getElementById("name").innerHTML = resume.name
+document.getElementById("name").innerHTML = resume.name;
 
+document.getElementById("summary").appendChild(p);
+document.getElementById("summary").lastChild.innerHTML = resume.summary;
 // List Contact info
 function contactInfo() {
   for (var key in resume.contact) {
     if (resume.contact.hasOwnProperty(key)) {
-      document.getElementById("contact").appendChild(`<li>${resume.contact[key]}</li>`) 
+      document.getElementById("contact").appendChild(document.createElement("LI"));
+      document.getElementById("contact").lastChild.classList.add("col-sm-1");
+      document.getElementById('contact').lastChild.innerHTML = resume.contact[key]; 
     }
   }
+}
+
+function skills() {
+  //this function will output my skills on the webpage. 
 }
 
 contactInfo()
